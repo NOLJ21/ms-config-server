ee3997d44126ea958b1d511ce498aeb754ef8958
NOLJ21 <nikolai.lopez@ucb.edu.bo>
2023-10-16 20:56:19 -0400

    servidor de configuraciones con cipher implementado

---
+++ src/test/java/com/example/severconfig/SeverConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.severconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeverConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
